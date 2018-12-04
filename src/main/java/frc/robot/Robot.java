57c091a8c04b4c2593cd9e80a82a1e52a98db8b9
Brenden Campbell <campbellbrendene@gmail.com>
2018-12-04 11:29:12 -0500

    Add SmartDashboard joystick movements to Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -13,13 +6,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.swerve.SwerveDrive;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends IterativeRobot {
   // Auton
   private static final String autonIDDefault = "Default";
@@ -88,6 +74,10 @@ public class Robot extends IterativeRobot {
 
   @Override
   public void teleopPeriodic() {
+    SmartDashboard.putNumber("Joystick X", joystickLeft.getX());
+    SmartDashboard.putNumber("Joystick Y", joystickLeft.getY());
+    SmartDashboard.putNumber("Joystick Z", joystickLeft.getZ());
+
     drivetrain.drive(joystickLeft.getX(), joystickLeft.getY(), joystickLeft.getZ());
   }
 
